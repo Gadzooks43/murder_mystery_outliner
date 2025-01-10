652f9358fc8a5037032ba1bab8cfcdfe2b12e46d
Isaac Kenney <kenneyisaac43@gmail.com>
2025-01-09 21:35:45 -0600

    finished models

--- murder_mystery_outliner/outliner/models.py
+++ murder_mystery_outliner/outliner/models.py
@@ -8,35 +8,52 @@ class Project(models.Model):
     date_created = models.DateTimeField(default=timezone.now)
     is_complete = models.BooleanField(default=False)
 
+    characters = models.ManyToManyField('Character', blank=True)
+    plot_a = models.ForeignKey('APlot', on_delete=models.CASCADE, related_name='a_plots', null=True, blank=True)
+    plot_b = models.ForeignKey('BPlot', on_delete=models.CASCADE, related_name='b_plots', null=True, blank=True)
+    plot_c = models.ForeignKey('CPlot', on_delete=models.CASCADE, related_name='c_plots', null=True, blank=True)
+    chapters = models.ManyToManyField('Chapter', blank=True)
+
     def __str__(self):
         return self.name
     
 class Character(models.Model):
     name = models.CharField(max_length=200)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField(default="")
+    background = models.TextField(default="")
+    alibi = models.TextField(default="")
+    secrets = models.TextField(default="")
+    motive = models.TextField(default="")
 
     def __str__(self):
         return self.name
 
 class Plot(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(default="")
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='plots')
+    characters = models.ManyToManyField(Character, blank=True)
 
     def __str__(self):
         return self.title
 
 class APlot(Plot):
-    pass
+    murder = models.TextField(default="")  # Specific to APlot
+    archetype = models.ForeignKey('Archetype', on_delete=models.CASCADE, null=True, blank=True)
+    cause_of_death = models.ForeignKey('CauseOfDeath', on_delete=models.CASCADE, null=True, blank=True)
+    red_herrings = models.TextField(default="")  # Specific to APlot
 
 class BPlot(Plot):
-    pass
+    connection_to_a_plot = models.TextField(default="")
 
 class CPlot(Plot):
-    pass
+    connection_to_a_plot = models.TextField(default="")
+    connection_to_b_plot = models.TextField(default="")
 
 class Chapter(models.Model):
-    description = models.TextField()
-    number = models.IntegerField()
+    description = models.TextField(default="")
+    number = models.IntegerField(null=False, blank=False)
     characters = models.ManyToManyField(Character)
 
     def __str__(self):
@@ -45,29 +62,29 @@ class Chapter(models.Model):
 
 class Archetype(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
-    examples = models.TextField()
+    description = models.TextField(default="")
+    examples = models.TextField(default="")
     
     def __str__(self):
         return self.name
 
 class CauseOfDeath(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(default="")
     
     def __str__(self):
         return self.name
     
 class Motives(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(default="")
     
     def __str__(self):
         return self.name
 
 class Role(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(default="")
     
     def __str__(self):
         return self.name
